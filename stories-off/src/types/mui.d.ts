f1841eb62e84c9ee47b1f8fb20a0503a6e388594
Nebulasse <stor1esoffica@gmail.com>
2025-05-02 23:34:55 +0300

    Update TypeScript configuration and add type declarations

---
+++ stories-off/src/types/mui.d.ts
@@ -0,0 +1,19 @@
+declare module '@mui/material' {
+  export * from '@mui/material';
+}
+
+declare module '@mui/icons-material/*' {
+  export * from '@mui/icons-material/*';
+}
+
+declare module '@mui/material/styles' {
+  export * from '@mui/material/styles';
+}
+
+declare module '@emotion/react' {
+  export * from '@emotion/react';
+}
+
+declare module '@emotion/styled' {
+  export * from '@emotion/styled';
+} 
